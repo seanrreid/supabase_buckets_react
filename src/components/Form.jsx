694907c8b9d863e39cbe3b41d1c322a5437a463b
Sean R Reid <sean@torchcodelab.com>
2024-03-22 10:02:42 -0400

    Added onchange to immediately upload image

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
 import supabase from '../supabase';
 
 const MyForm = () => {
+    const [profilePhotoId, setProfilePhotoId] = useState(null);
     const handleUpload = async (photo) => {
         const fileName = photo.name;
         const { data, error } = await supabase.storage
             .from('profile-photos')
             .upload(`test/${fileName}`, photo);
         console.log('data, or error', data, error);
+        setProfilePhotoId(data.id);
+    };
+
+    const handleChange = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target.form);
+        const photo = formData.get('photo2');
+        return handleUpload(photo);
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
-        const photo = formData.get('photo2');
-        return handleUpload(photo);
+        formData.append('photo', profilePhotoId);
+        // Display the key/value pairs
+        for (const pair of formData.entries()) {
+            console.log(pair[0], pair[1]);
+        }
+        return;
     };
 
     return (
@@ -25,7 +40,7 @@ const MyForm = () => {
             </p>
             <form onSubmit={handleSubmit}>
                 <label htmlFor='photo2'>File: </label>
-                <input type='file' name='photo2' />
+                <input type='file' name='photo2' onChange={handleChange} />
                 <button type='submit'>Submit</button>
             </form>
         </>
